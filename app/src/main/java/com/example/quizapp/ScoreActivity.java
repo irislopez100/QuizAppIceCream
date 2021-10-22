1d01c9ac676cfbb9b505dcc8ad20ded5bae5e8fa
Lopez Ayala <1334830@bcpss.org>
2021-10-22 15:04:48 -0400

    i learned how to create a new activity

---
+++ app/src/main/java/com/example/quizapp/ScoreActivity.java
@@ -0,0 +1,28 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    TextView scoreTV;
+    int score;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        scoreTV = (TextView) findViewById(R.id.ScoreTV);
+
+        Intent incomingIntent = getIntent();
+        score = incomingIntent.getIntExtra("score", 0);
+
+        scoreTV.setText(""+score);
+
+    }
+}
